1264f28eb0cb96ab7ec44ecb09c5d50eef508ec5
Jan Mrázek <email@honzamrazek.cz>
2024-05-03 08:07:40 +0200

    Upgrade versioneer

--- pcbdraw/__init__.py
+++ pcbdraw/__init__.py
@@ -2,3 +2,6 @@
 from ._version import get_versions
 __version__ = get_versions()['version'] # type: ignore
 del get_versions
+
+from . import _version
+__version__ = _version.get_versions()['version']
